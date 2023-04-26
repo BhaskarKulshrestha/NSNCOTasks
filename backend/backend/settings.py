ef772d433fbc6d11efc0a3abecb583866ca431da
Bhaskar Kulshrestha <95305804+BhaskarKulshrestha@users.noreply.github.com>
2023-04-26 23:21:35 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,1 +1,36 @@
+# Application definition
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'api',
+]
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# REST Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+}
+
+# Add the following at the end of the file
+AUTH_USER_MODEL = 'api.User'
+
+# The rest of the settings.py file remains unchanged.
 
